bfbb97c7b6be18f4c22782c57567d938489c22f3
raihanwebmaster <raihanemon2015@gmail.com>
2022-12-26 20:14:13 +0100

    setup react router dom and layout and error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import AuthLayout from "./layouts/AuthLayout";
+import MainLayout from "./layouts/MainLayout";
+import Error from "./pages/Error";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Menu from "./pages/Menu";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "menu",
+        element: <Menu />,
+      },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className='bg-gray-900'>
+    {/* <div className='bg-gray-900'>
     <App />
-    </div>
-  </React.StrictMode>,
-)
+    </div> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Table from "../components/Table";
+import { TABLE_STATUS } from "../enums";
+
+const tables = [
+  {
+    id: 1,
+    tableNumber: 1,
+    status: TABLE_STATUS.AVAILABLE,
+  },
+  {
+    id: 2,
+    tableNumber: 2,
+    status: TABLE_STATUS.OCCUPIED,
+  },
+  {
+    id: 3,
+    tableNumber: 3,
+    status: TABLE_STATUS.AVAILABLE,
+  },
+];
+
+const HomePage = () => {
+  return (
+    <div className="flex gap-2">
+      {tables.map((table) => (
+        <Table
+          key={table.id}
+          tableNumber={table.tableNumber}
+          status={table.status}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
